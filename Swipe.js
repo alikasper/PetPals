89741add5f24ea6bd713bf604914ddb282ebfcce
Alexandra Kasper <alexandrakasper@Alis-MacBook-Pro.local>
2020-09-07 18:19:57 -0400

    update styles

--- Swipe.js
+++ Swipe.js
@@ -1,12 +1,38 @@
 $().ready(() => {
   console.log("Jquery is loading...");
-  fetch("https://api.thecatapi.com/v1/images/search")
-    .then((response) => response.json())
+  // fetch("https://api.thecatapi.com/v1/images/search")
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log("catimage is loading...");
+  //     console.log(data[0].url);
+  //     $("#draggable").append(`
+  //       <img src="${data[0].url}" />
+  //       `);
+  //   });
+
+  function catSwipe() {
+    authFunc()
     .then((data) => {
-      console.log("catimage is loading...");
-      console.log(data[0].url);
-      $("#draggable").append(`
-        <img src="${data[0].url}" />
-        `);
-    });
-});
+        console.log(data)
+        let token = data.access_token
+        fetch("https://api.petfinder.com/v2/animals?type=cat", {
+            method: 'get',
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        .then(response => response.json())
+        .then((data) => {
+          console.log(data);
+          $("#draggable").append(`
+          <img src="${imageSrc(data.animals[0].photos)}"/>
+          <h3>${data.animals[0].name}</h3>
+          `);         
+        })
+    })
+  }
+
+  catSwipe();
+  $("#draggable").draggable({axis: "x"});
+
+})
--- matched-list.js
+++ matched-list.js
@@ -1,58 +1,55 @@
 $(document).ready(() => {
-    fetch("https://api.petfinder.com/v2/oauth2/token", {
-        method: 'post',
-        body: JSON.stringify({
-            grant_type: "client_credentials",
-            client_id: process.env.pfapiKey,
-            client_secret: process.env.pfapiSecret
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then((data) => {
-        console.log(data)
-    
-        let token = data.access_token
-        fetch("https://api.petfinder.com/v2/animals?type=cat", {
-            method: 'get',
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        })
-        .then(response => response.json())
-        .then((data) => {
-            console.log(data.animals)
-            function renderPetPals(catsList) {
-                let noPhotoAvail = "images/no_image.png";
-                let imageSrc = function(array) {
-                    if (array.length === 0) {
-                        return noPhotoAvail;
-                    }
-                    else {
-                        return array[0].large;
-                    }
-                }
 
-                let petHTMLs = catsList.map((currentCat) => {
-                    return `<div class="pet-card" style="width: 300px;">
-                <img class="pet-card-img-top" src="${imageSrc(currentCat.photos)}" alt="Pet card image cap" height="300px" width="200px">
-                        <div class="pet-card-body">
-                            <h5 class="pet-card-name"><b>Name:</b> ${currentCat.name}</h5>
-                            <p class="pet-card-bio"><b>Bio:</b> ${currentCat.description}</p>
-                            <p class="pet-card-breed"><b>Primary Breed:</b> ${currentCat.breeds.primary}</p>
-                            <p class="pet-card-color"><b>Primary Color:</b> ${currentCat.colors.primary} </p>
-                            <p class="pet-card-age"><b>Age:</b> ${currentCat.age}</p>
-                            <a class="pet-card-pf-listing" href="${currentCat.url}">Go to the Original PetFinder Listing</a>
-                        <button type="button" class="remove-button">Remove!</button>
-                        </div>
-                    </div>`
+    function renderCatCards() {
+        authFunc()
+        .then((data) => {
+            console.log(data)
         
-                })
-                return petHTMLs.join("");
-            }
-            $(".pet-pals-container").html(renderPetPals(data.animals));
+            let token = data.access_token
+            fetch("https://api.petfinder.com/v2/animals?type=cat", {
+                method: 'get',
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            })
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data.animals)
+                function renderPetPals(catsList) {
+
+                    let petHTMLs = catsList.map((currentCat) => {
+                        console.log(currentCat);
+                        return `<div class="pet-card" style="width: 300px;">
+                            <img class="pet-card-img-top" src="${imageSrc(currentCat.photos)}" alt="Pet card image cap" height="300px" width="200px">
+                            <div class="pet-card-body">
+                                <h5 class="pet-card-name"><b>Name:</b> ${currentCat.name}</h5>
+                                <p class="pet-card-bio"><b>Bio:</b> ${currentCat.description}</p>
+                                <p class="pet-card-breed"><b>Primary Breed:</b> ${currentCat.breeds.primary}</p>
+                                <p class="pet-card-color"><b>Primary Color:</b> ${currentCat.colors.primary} </p>
+                                <p class="pet-card-age"><b>Age:</b> ${currentCat.age}</p>
+                                <a class="pet-card-pf-listing" href="${currentCat.url}">Go to the Original PetFinder Listing</a>
+                            <button type="button" class="remove-button">Remove!</button>
+                            </div>
+                        </div>`
+            
+                    })
+                    return petHTMLs.join("");
+                }
+                $(".pet-pals-container").html(renderPetPals(data.animals));
+            })
         })
-    })  
+    }
+
+    renderCatCards();   
+
 })
+
+let noPhotoAvail = "images/no_image.png";
+let imageSrc = function(array) {
+    if (array.length === 0) {
+        return noPhotoAvail;
+    }
+    else {
+        return array[0].large;
+    }
+}
