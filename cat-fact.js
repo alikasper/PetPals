961849f7d6839da9e51d27c1afbff229e6f55d63
Alexandra Kasper <alexandrakasper@Alis-MacBook-Pro.local>
2020-09-10 19:37:34 -0400

    cat-fact js

---
+++ cat-fact.js
@@ -0,0 +1,7 @@
+let catFact = document.getElementById("cat-fact");
+
+fetch("https://catfact.ninja/fact?max_length=140") 
+.then(response => response.json())
+    .then((data) => {
+        catFact.innerHTML = data.fact;
+    })
