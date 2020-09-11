390ac493ce1f4ad2c0085b44cd7713a01a573947
Rudy Saavedra <RudyCruisin@outlook.com>
2020-09-11 12:50:57 -0400

    for matt to look at

---
+++ mapauth.js
@@ -0,0 +1,25 @@
+// function initMap() {
+//     var mapOptions = {
+//         zoom: 4,
+//         center: new google.maps.LatLng(40.903145, -99.23366)
+//     };
+//     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+// }
+
+// function loadScript() {
+//     var script = document.createElement('script');
+//     script.type = 'text/javascript';
+//     script.src = `https://maps.googleapis.com/maps/api/js?key=${gmapsKey}&callback=initMap`
+//     defer = true;
+//     document.body.appendChild(script);
+// }
+// window.onload = loadScript;
+
+// var script = document.createElement('script');
+// script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAXJ-vtESufpQES_Z3AtBL8AlkBxNpl5mU&callback=initMap`;
+// script.defer = true;
+
+// window.initMap = function() {
+//   };
+//   document.head.appendChild(script);
+  
