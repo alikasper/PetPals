fb7512ecce0dd15625a716e2233964385926cd83
Rudy Saavedra <RudyCruisin@outlook.com>
2020-09-12 10:59:16 -0400

    map functionality added

--- map.js
+++ map.js
@@ -9,22 +9,26 @@ $().ready(() => {
       zoom: 4,
       center: mapcenter,
     });
+    // var map;
   
-    async function renderMarkers(animallist) {
-      // var script = document.createElement("script");
-      // script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAXJ-vtESufpQES_Z3AtBL8AlkBxNpl5mU&callback=initMap`;
-      // script.defer = true;
+    // async function renderMap(animallist) {
+    //   var script = document.createElement("script");
+    //   script.src = `https://maps.googleapis.com/maps/api/js?key=${gmapsKey}&callback=initMap`;
+    //   script.defer = true;
   
-      // window.initMap = function () {
-      //   var mapcenter = { lat: 40.903145, lng: -99.23366 };
-      //   var map = new google.maps.Map(document.getElementById("map"), {
-      //     zoom: 4,
-      //     center: mapcenter,
-      //   });
-      // };
-      // document.head.appendChild(script);
-  
+    //   document.body.appendChild(script);
+    //   window.initMap = function () {
+    //     var mapcenter = { lat: 40.903145, lng: -99.23366 };
+    //     var map = new google.maps.Map(document.getElementById("map"), {
+    //       zoom: 4,
+    //       center: mapcenter,
+    //     });
+    //   };
+    // }
+
+    function renderMarkers(animallist) {
       var location = petlist.map((currentanimal) => {
+        let peturl = currentanimal.url;
         console.log (currentanimal)
         fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${currentanimal.contact.address.city},+${currentanimal.contact.address.state}&key=${geoKey}
           `)
@@ -33,16 +37,22 @@ $().ready(() => {
           //   console.log(data);
             console.log(currentanimal.name + " is located at: " +data.results[0].formatted_address);
             console.log(data.results[0].geometry.location);
-            new google.maps.Marker({
+            var marker = new google.maps.Marker({
               position: data.results[0].geometry.location,
               map: map,
               title: currentanimal.name,
-              label: currentanimal.name.charAt(0),
-            //   icon: currentanimal.photos[0].small,
-              animation: google.maps.Animation.DROP
+            //   label: currentanimal.name.charAt(0),
+              icon: currentanimal.photos[0].small,
+              animation: google.maps.Animation.DROP,
+              url: peturl
             });
+            google.maps.event.addListener(marker, 'click', function() {
+                window.open(this.url,"_blank");
+            })
           });
       });
     }
+    // renderMap();
     renderMarkers();
+
   });
