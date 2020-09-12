7af9d245c670f2100ca9f9acadecf57940a68b33
Rudy Saavedra <RudyCruisin@outlook.com>
2020-09-12 12:24:21 -0400

    prettier applied to code

--- image-source.js
+++ image-source.js
@@ -1,9 +1,8 @@
 let noPhotoAvail = "images/no_image.png";
-let imageSrc = function(array) {
-    if (array.length === 0) {
-        return noPhotoAvail;
-    }
-    else {
-        return array[0].full;
-    }
-}
+let imageSrc = function (array) {
+  if (array.length === 0) {
+    return noPhotoAvail;
+  } else {
+    return array[0].full;
+  }
+};
--- remove-card.js
+++ remove-card.js
@@ -1,15 +1,13 @@
-function removeCard(btn){
-    const animalID = btn.getAttribute("data-animalID");
-    ((btn.parentNode).parentNode.parentNode).removeChild(btn.parentNode.parentNode);
-    removeInfo(animalID);
+function removeCard(btn) {
+  const animalID = btn.getAttribute("data-animalID");
+  btn.parentNode.parentNode.parentNode.removeChild(btn.parentNode.parentNode);
+  removeInfo(animalID);
 }
 
 function removeInfo(id) {
-    
-    var retrievedData = localStorage.getItem("petlist");
+  var retrievedData = localStorage.getItem("petlist");
 
-    var petlist2 = JSON.parse(retrievedData).filter(pet => pet.id != id);
-    
-    localStorage.setItem('petlist', JSON.stringify(petlist2));
+  var petlist2 = JSON.parse(retrievedData).filter((pet) => pet.id != id);
 
+  localStorage.setItem("petlist", JSON.stringify(petlist2));
 }
