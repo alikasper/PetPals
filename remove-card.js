ec7562efcf16637407b68b07d8417b6d9d53ad1f
Alexandra Kasper <alexandrakasper@Alis-MacBook-Pro.local>
2020-09-12 09:06:49 -0400

    remove button works

--- remove-card.js
+++ remove-card.js
@@ -1,3 +1,15 @@
 function removeCard(btn){
+    const animalID = btn.getAttribute("data-animalID");
     ((btn.parentNode).parentNode.parentNode).removeChild(btn.parentNode.parentNode);
+    removeInfo(animalID);
+}
+
+function removeInfo(id) {
+    
+    var retrievedData = localStorage.getItem("petlist");
+
+    var petlist2 = JSON.parse(retrievedData).filter(pet => pet.id != id);
+    
+    localStorage.setItem('petlist', JSON.stringify(petlist2));
+
 }
